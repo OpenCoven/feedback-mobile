5c664b962560f60c6c25ffd46f9cd513adbb4bf5
James Morton <james.morton@quackback.io>
2026-04-02 09:48:28 +0100

    feat: use full-screen presentation instead of page sheet

--- Sources/Quackback/Internal/PanelController.swift
+++ Sources/Quackback/Internal/PanelController.swift
@@ -7,23 +7,21 @@ final class PanelController: UIViewController {
 
     init(webViewManager: QuackbackWebView) {
         self.webViewManager = webViewManager; super.init(nibName: nil, bundle: nil)
-        modalPresentationStyle = .pageSheet
+        modalPresentationStyle = .fullScreen
     }
     @available(*, unavailable) required init?(coder: NSCoder) { fatalError() }
 
     override func viewDidLoad() {
         super.viewDidLoad(); webViewManager.loadIfNeeded()
+        view.backgroundColor = .systemBackground
         guard let wv = webViewManager.webView else { return }
         wv.translatesAutoresizingMaskIntoConstraints = false; view.addSubview(wv)
         NSLayoutConstraint.activate([
-            wv.topAnchor.constraint(equalTo: view.topAnchor),
+            wv.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             wv.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             wv.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             wv.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
-        if let sheet = sheetPresentationController {
-            sheet.detents = [.medium(), .large()]; sheet.prefersGrabberVisible = true
-        }
     }
 
     override func viewDidDisappear(_ animated: Bool) {
